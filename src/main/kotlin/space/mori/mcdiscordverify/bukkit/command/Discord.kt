ba35e30958b7e745c6bfeb495195807ef5d56d55
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2024-07-24 13:23:52 +0900

    update README.md, add reload command

--- src/main/kotlin/space/mori/mcdiscordverify/bukkit/command/Discord.kt
+++ src/main/kotlin/space/mori/mcdiscordverify/bukkit/command/Discord.kt
@@ -3,6 +3,8 @@ package space.mori.mcdiscordverify.bukkit.command
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import space.mori.mcdiscordverify.bukkit.MCDiscordVerify
+import space.mori.mcdiscordverify.bukkit.MCDiscordVerify.Companion.instance
 import space.mori.mcdiscordverify.bukkit.config.Language.prefix
 import space.mori.mcdiscordverify.bukkit.config.getDiscordUser
 import space.mori.mcdiscordverify.utils.sendColorMessage
@@ -16,14 +18,32 @@ object Discord: CommandBase(
             "mcdiscordverify.discord"
         ) {
             override fun commandExecutor(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-                /* val member = (sender as Player).getDiscordUser
+                val member = (sender as Player).getDiscordUser
 
                 if (member != null) {
                     sender.sendColorMessage("$prefix Your discord account is &9${member.nickname}&r (&6@${member.user.name}&r)")
                 } else {
                     sender.sendColorMessage("$prefix Your discord account is not found!")
-                } */
-                sender.sendColorMessage("$prefix testing...")
+                }
+
+                return true
+            }
+        },
+        object: SubCommand(
+            "reload",
+            "reload this plugin",
+            "",
+            "mcdiscordverify.reload"
+        ) {
+            override fun commandExecutor(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+                MCDiscordVerify.pluginConfig.load()
+                MCDiscordVerify.uuidToDiscordID.load()
+                MCDiscordVerify.language.load()
+                MCDiscordVerify.discordHandler.disable()
+                instance.server.scheduler.runTaskLater(instance, Runnable {
+                    MCDiscordVerify.discordHandler.main()
+                    sender.sendColorMessage("$prefix Reload complete.")
+                }, 20L * 10)
 
                 return true
             }
--- src/main/kotlin/space/mori/mcdiscordverify/bungee/command/Discord.kt
+++ src/main/kotlin/space/mori/mcdiscordverify/bungee/command/Discord.kt
@@ -2,10 +2,11 @@ package space.mori.mcdiscordverify.bungee.command
 
 import net.md_5.bungee.api.CommandSender
 import net.md_5.bungee.api.connection.ProxiedPlayer
-import space.mori.mcdiscordverify.bukkit.config.Language
+import space.mori.mcdiscordverify.bungee.MCDiscordVerify
+import space.mori.mcdiscordverify.bungee.MCDiscordVerify.Companion.instance
 import space.mori.mcdiscordverify.bungee.config.Language.prefix
 import space.mori.mcdiscordverify.bungee.config.getDiscordUser
-import space.mori.mcdiscordverify.utils.sendColorMessage
+import java.util.concurrent.TimeUnit
 
 object Discord: CommandBase(
     mutableListOf(
@@ -15,22 +16,41 @@ object Discord: CommandBase(
             ""
         ) {
             override fun commandExecutor(sender: CommandSender, args: Array<out String>): Boolean {
-                /* val member = (sender as ProxiedPlayer).getDiscordUser
+                val member = (sender as ProxiedPlayer).getDiscordUser
 
                 if(member != null) {
-                    sendMessage(sender, "${Language.prefix} Your discord account is &9${member.effectiveName}&r (&6@${member.user.name}&r)")
+                    sendMessage(sender, "$prefix Your discord account is &9${member.effectiveName}&r (&6@${member.user.name}&r)")
                 } else {
                     sendMessage(sender, "$prefix Your discord account is not found!")
-                } */
-                sendMessage(sender, "${Language.prefix} testing...")
+                }
+                // sendMessage(sender, "${Language.prefix} testing...")
+
+                return true
+            }
+        },
+        object: SubCommand(
+            "reload",
+            "reload this plugin",
+            "",
+            "mcdiscordverify.reload"
+        ) {
+            override fun commandExecutor(sender: CommandSender, args: Array<out String>): Boolean {
+                MCDiscordVerify.pluginConfig.load()
+                MCDiscordVerify.uuidToDiscordID.load()
+                MCDiscordVerify.language.load()
+                MCDiscordVerify.discordHandler.disable()
+                instance.proxy.scheduler.schedule(instance, {
+                    MCDiscordVerify.discordHandler.main()
+                    sendMessage(sender, "$prefix Reload complete.")
+                }, 20L * 10, TimeUnit.SECONDS)
 
                 return true
             }
         }
     ).associateBy { it.name }.toSortedMap(),
     "discord",
-    "mcdiscordverify.discord",
-    arrayOf("")
+    null,
+    null
 ) {
     override fun onCommand(sender: CommandSender, args: Array<out String>) {
         if (args.isEmpty() || args[0] == "help" || !SubCommands.keys.contains(args[0])) {
